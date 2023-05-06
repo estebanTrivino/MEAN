2e4172536aaf009c843217e5e3b76891206763dc
Esteban Trivino <jorge.trivino@pysltda.com>
2023-05-06 15:02:54 -0500

    FEAT:
    Actualización front-End.

--- cliente/src/app/components/crear-producto/crear-producto.component.ts
+++ cliente/src/app/components/crear-producto/crear-producto.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Producto} from "../../models/producto";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 
 @Component({
   selector: 'app-crear-producto',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-producto.component.css']
 })
 export class CrearProductoComponent {
+  productoForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router, private toastr: ToastrService) {
+    this.productoForm = this.fb.group({
+      producto: ['', Validators.required],
+      categoria: ['', Validators.required],
+      ubicacion: ['', Validators.required],
+      precio: ['', Validators.required]
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+  agregarProducto(){
+    const PRODUCTO: Producto = {
+      nombre: this.productoForm.get('producto')?.value,
+      categoria: this.productoForm.get('categoria')?.value,
+      ubicacion: this.productoForm.get('ubicacion')?.value,
+      precio: this.productoForm.get('precio')?.value
+    }
+    this.toastr.success('El producto fue agregado con exito.', 'Producto Agregado');
+    this.router.navigate(['/']);
+  }
 }
